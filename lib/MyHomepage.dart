0e362256fc69c8cd6d85092bee2b01306734f430
AhmedRizwan23 <ahmedrixwan@gmail.com>
2024-01-01 14:20:01 +0500

    ..

--- lib/MyHomepage.dart
+++ lib/MyHomepage.dart
@@ -1,6 +1,7 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:videoplayer/assets/assets_path.dart';
 
 class Feautredpage extends StatefulWidget {
   const Feautredpage({Key? key}) : super(key: key);
@@ -11,33 +12,45 @@ class Feautredpage extends StatefulWidget {
 
 class _IntropageState extends State<Feautredpage> {
   late VideoPlayerController videoPlayerController;
-  late ChewieController chewieController;
+  ChewieController? chewieController;
 
   @override
   void initState() {
     super.initState();
-    videoPlayerController =
-        VideoPlayerController.asset("lib/assets/introvideo.mp4");
-    chewieController = ChewieController(
-      videoPlayerController: videoPlayerController,
-      autoPlay: true,
-      looping: true,
-    );
+    videoPlayerController = VideoPlayerController.asset(Assetpath.testvideo);
+
+    videoPlayerController.initialize().then((value) {
+      if (videoPlayerController.value.duration <= const Duration(minutes: 1)) {
+        setState(() {
+          chewieController = ChewieController(
+            videoPlayerController: videoPlayerController,
+            autoPlay: true,
+            looping: true,
+          );
+        });
+      } else {
+        print("Video duration exceds 1 minute");
+      }
+    });
   }
 
   @override
   void dispose() {
     super.dispose();
     videoPlayerController.dispose();
-    chewieController.dispose();
+    chewieController?.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Chewie(
-        controller: chewieController,
-      ),
+      body: chewieController != null
+          ? Chewie(
+              controller: chewieController!,
+            )
+          : const Center(
+              child: Text("Video duration exceeds 1 minute"),
+            ),
     );
   }
 }
---
+++ lib/assets/assets_path.dart
@@ -0,0 +1,4 @@
+class Assetpath {
+  static var introvideo = "lib/assets/introvideo.mp4";
+  static var testvideo = "lib/assets/Test.mp4";
+}
