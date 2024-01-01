85ac870e1906d7fb59acfc7f9a902252c0a3c0b6
AhmedRizwan23 <ahmedrixwan@gmail.com>
2024-01-02 00:38:27 +0500

    ..

--- lib/MyHomepage.dart
+++ lib/MyHomepage.dart
@@ -1,56 +1,122 @@
-import 'package:chewie/chewie.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-import 'package:videoplayer/assets/assets_path.dart';
+import 'package:chewie/chewie.dart';
+import 'package:image_picker/image_picker.dart';
 
-class Feautredpage extends StatefulWidget {
-  const Feautredpage({Key? key}) : super(key: key);
+class newlogic extends StatefulWidget {
+  const newlogic({Key? key}) : super(key: key);
 
   @override
-  State<Feautredpage> createState() => _IntropageState();
+  State<newlogic> createState() => _newlogicState();
 }
 
-class _IntropageState extends State<Feautredpage> {
+class _newlogicState extends State<newlogic> {
   late VideoPlayerController videoPlayerController;
   ChewieController? chewieController;
-
-  @override
-  void initState() {
-    super.initState();
-    videoPlayerController = VideoPlayerController.asset(Assetpath.testvideo);
-
-    videoPlayerController.initialize().then((value) {
-      if (videoPlayerController.value.duration <= const Duration(minutes: 1)) {
-        setState(() {
-          chewieController = ChewieController(
-            videoPlayerController: videoPlayerController,
-            autoPlay: true,
-            looping: true,
-          );
-        });
-      } else {
-        print("Video duration exceds 1 minute");
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    videoPlayerController.dispose();
-    chewieController?.dispose();
-  }
+  bool videouploading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: chewieController != null
-          ? Chewie(
-              controller: chewieController!,
-            )
-          : const Center(
-              child: Text("Video duration exceeds 1 minute"),
-            ),
+      body: Center(
+        child: chewieController != null
+            ? Chewie(
+                controller: chewieController!,
+              )
+            : Center(
+                child: videouploading
+                    ? const CircularProgressIndicator()
+                    : const Text("Upload short video")),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _pickVideo(context), // Pass context to _pickVideo
+        child: const Icon(Icons.upload),
+      ),
     );
+  }
+
+  Future<void> _pickVideo(BuildContext context) async {
+    try {
+      final picker = ImagePicker();
+      var pickedFile = await picker.pickVideo(
+          source: ImageSource.gallery,
+          maxDuration: const Duration(seconds: 60));
+      if (pickedFile == null) {
+        return;
+      }
+
+      VideoPlayerController testLengthController =
+          VideoPlayerController.file(File(pickedFile.path));
+      await testLengthController.initialize();
+
+      if (testLengthController.value.duration.inSeconds > 60) {
+        pickedFile = null;
+        _showVideoDurationDialog(
+            context); // Show dialog on the file picker screen
+      } else {
+        setState(() {
+          videoPlayerController =
+              VideoPlayerController.file(File(pickedFile!.path));
+          _startVideoPlayer();
+        });
+      }
+      testLengthController.dispose();
+    } catch (e) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              content: Text(e.toString()),
+            );
+          });
+      return;
+    }
+  }
+
+  void _showVideoDurationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const AlertDialog(
+          backgroundColor: Colors.black,
+          content: Text(
+            'We only allow videos that are shorter than 1 minute!',
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      },
+    ).then((value) {
+      // This will be executed after the dialog is dismissed
+      _pickVideo(context);
+    });
+  }
+
+  void _startVideoPlayer() async {
+    try {
+      await videoPlayerController.initialize();
+
+      setState(() {
+        chewieController = ChewieController(
+          videoPlayerController: videoPlayerController,
+          autoPlay: true,
+          looping: true,
+        );
+      });
+    } catch (error) {
+      print("Error initializing video: $error");
+    } finally {
+      setState(() {
+        videouploading = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController?.dispose();
+    super.dispose();
   }
 }
--- lib/selectvideo.dart
+++ lib/selectvideo.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
-import 'package:chewie/chewie.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
 class NewHompage extends StatefulWidget {
   const NewHompage({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<NewHompage> createState() => _NewHompageState();
@@ -50,33 +51,91 @@ class _NewHompageState extends State<NewHompage> {
     );
     if (result != null) {
       String filePath = result.files.single.path!;
-      await _initializeVideo(filePath);
+      final FlutterFFprobe flutterFFprobe = FlutterFFprobe();
+
+      var mediaInfo = await flutterFFprobe.getMediaInformation(filePath);
+      double durationInSeconds = double.parse(
+        mediaInfo.getMediaProperties()?['duration'] ?? '0',
+      );
+      Duration duration = Duration(seconds: durationInSeconds.round());
+
+      if (duration > const Duration(minutes: 1)) {
+        // Display an error message when the video exceeds 1 minute length
+        Get.snackbar(
+          "Upload error",
+          "Video exceeds 1 minute length",
+          snackPosition: SnackPosition.BOTTOM,
+        );
+
+        print("Video exceeds 1 minute length");
+        _uploadVideo();
+      } else {
+        // Proceed to initialize video
+        await initializeVideo(filePath);
+      }
+
+      // User canceled file selection or video exceeded 1 minute
+      setState(() {
+        videouploading = false;
+      });
     }
   }
+  // Future<void> _checkVideoDuration(String filePath) async {
+  //   final FlutterFFprobe flutterFFprobe = FlutterFFprobe();
 
-  Future<void> _initializeVideo(String filePath) async {
+  //   try {
+  //     var mediaInfo = await flutterFFprobe.getMediaInformation(filePath);
+  //     double durationInSeconds = double.parse(
+  //       mediaInfo.getMediaProperties()?['duration'] ?? '0',
+  //     );
+  //     Duration duration = Duration(seconds: durationInSeconds.round());
+
+  //     if (duration > const Duration(minutes: 1)) {
+  //       // Display an error message when the video exceeds 1 minute length
+  //       Get.snackbar(
+  //         "Upload error",
+  //         "Video exceeds 1 minute length",
+  //         snackPosition: SnackPosition.BOTTOM,
+  //       );
+  //       print("Video exceeds 1 minute length");
+  //       _uploadVideo();
+  //       setState(() {
+  //         videouploading = false;
+  //       });
+  //     } else {
+  //       await _initializeVideo(filePath);
+  //     }
+  //   } catch (error) {
+  //     print("Error getting video information: $error");
+  //     // Handle the error as needed
+  //     setState(() {
+  //       videouploading = false;
+  //     });
+  //   }
+  // }
+
+  Future<void> initializeVideo(String filePath) async {
     videoPlayerController = VideoPlayerController.file(File(filePath));
 
     try {
       await videoPlayerController.initialize();
-      if (videoPlayerController.value.duration <= const Duration(minutes: 1)) {
-        setState(() {
-          chewieController = ChewieController(
-            videoPlayerController: videoPlayerController,
-            autoPlay: true,
-            looping: true,
-          );
-        });
-      } else {
-        Get.snackbar("Upload error", "Video exceeds 1 minute length",
-            snackPosition: SnackPosition.BOTTOM);
-      }
+
+      // Display the video preview
+      setState(() {
+        chewieController = ChewieController(
+          videoPlayerController: videoPlayerController,
+          autoPlay: true,
+          looping: true,
+        );
+      });
     } catch (error) {
       print("Error initializing video: $error");
+    } finally {
+      // Set uploading state to false after completing the initialization
+      setState(() {
+        videouploading = false;
+      });
     }
-    setState(() {
-      videouploading = false;
-    });
   }
 
   @override
